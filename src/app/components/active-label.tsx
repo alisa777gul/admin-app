624f3ae63f98088413e2445088404bab2d0aed34
aliso4ka <gulyayevaalisa@gmail.com>
2025-03-19 19:31:29 +0100

    idk

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import styles from './active-label.module.css';
 
-export interface ActiveLabelProps{
-    children: React.ReactNode;
+export enum Status{
+    Active = 'Active',
+    NotActive = 'NotActive',
+    Pending = 'Pending',
+    Suspended = 'Suspended'
 }
 
-export default function ActiveLabel({ children }: ActiveLabelProps) {
-    return (<span className={styles.label}>{children}</span>);
+export interface StatusLabelProps{
+    children: React.ReactNode;
+    status: Status;
+}
+
+export default function StatusLabel({ children,status }: StatusLabelProps) {
+    return (<div  className={`inline-flex items-center py-1 px-3.5 rounded-3xl font-medium ${
+                status === Status.Active ? 'text-green-700 bg-green-100' :
+                status === Status.NotActive ? 'text-red-700 bg-red-100' :
+                status === Status.Pending ? 'text-orange-700 bg-orange-100' :
+                status === Status.Suspended ? 'text-blue-700 bg-blue-100' : ''
+            }`}><div className="w-1 h-1 mr-2 rounded-full bg-current"/>{children}</div>);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import ActiveLabel from "./components/active-label";
-import NotActiveLabel from "./components/not-active-label";
+import StatusLabel, { Status } from "./components/active-label";
+
 
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
-      <ActiveLabel>Active</ActiveLabel>
-      <NotActiveLabel>Not active</NotActiveLabel>
+      <h1 className="text-m">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Action</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+
 
     </main>
   )
